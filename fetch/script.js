b62cd587e8a5ce8417ec5e0cd256f36ad3f4c7f4
Theo02-12 <mrcouvelard@gmail.com>
2023-06-09 14:15:32 +0200

    nice

--- fetch/script.js
+++ fetch/script.js
@@ -1,10 +1,36 @@
 const title = document.querySelector('#name');
-fetch('http://localhost:3000')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        title.innerHTML = `Bonjour ${data.name}`;
-        const img = new Image();
-        img.src = data.img;
-        document.body.appendChild(img);
-    })
+const container = document.querySelector('#row')
+const showArticle = document.querySelector('#showArticle')
+let count = 0;
+showArticle.addEventListener('click', () => {
+
+    count++;
+    if (count == 1) {
+        fetch('http://localhost:3000/api')
+            .then(res => res.json())
+            .then(data => {
+                data.articles.forEach(element => {
+                    console.log(element);
+                    const srcImg = "http://127.0.0.1:5500/" + element.img;
+                    container.innerHTML += `
+                        <div class="row">
+                            <div class="col-10">
+                                <div class="card mx-3 d-flex flex-row mw-50"">
+                                    <img src="${srcImg}" class="card-img-top w-25 object-fit-cover" alt="...">
+                                    <div class="card-body ">
+                                    <h5 class="card-title">${element.name}</h5>
+                                    <p class="card-text">${element.description}</p>
+                                    <a href="#" class="btn btn-primary">Acheter</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <i class="bi bi-hand-thumbs-up text-primary"></i>
+                                <i class="bi bi-hand-thumbs-down"></i>
+                            </div>
+                        </div>`;
+                });
+            })
+    }
+
+})
--- node-js/index.js
+++ node-js/index.js
@@ -3,13 +3,15 @@ const { readFileSync, writeFileSync } = require('fs');
 const cors = require('cors')
 const app = express() // initialize app
 const port = 3000
+const apiRoot = '/api'
 
 // GET callback function returns a response message
 app.use(cors());
-
-app.get('/', (req, res) => {
+const router = express.Router();
+router.get('/', (req, res) => {
     res.send(readFileSync('./info.json', 'utf8'))
 })
+app.use(apiRoot, router)
 
 app.listen(port, () => {
 console.log(`Server listening at http://localhost:${port}`)
